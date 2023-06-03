b75bf34102d1b2915c845b86fff987a60bf50d16
Gabriel <murariugabi23@gmail.com>
2023-06-03 09:03:32 +0300

    Learned about initial rendering and triggering re-render

--- src/useState/UseStateBasics.jsx
+++ src/useState/UseStateBasics.jsx
@@ -10,23 +10,39 @@ const plusOne = () => {
 
   return (
     <section>
-        <h2>Use state Basics</h2>
+      <h2>Use state Basics</h2>
 
-        <p>
-            UseState is a destructured array which contains 2 things: <br /><br />
-            - The state we pass <br />
-            - A function that updates state
-        </p>
+      <p>
+        UseState is a destructured array which contains 2 things: <br />
+        <br />
+        - The state we pass <br />- A function that updates state
+      </p>
 
-        <p>Here we have a button which upon pressing is going to count how many times we have clicked  <br />
+      <p>
+        Here we have a button which upon pressing is going to count how many
+        times we have clicked <br />
         Inside this component we have a function which updates the state
-        </p>
-        
-        <p>You clicked {count} times</p>
+      </p>
 
-        <button onClick={plusOne}>CLICK ME</button>
+      <p>You clicked {count} times</p>
 
-        <p>{`"A slight note tho, you should probably not declare useState with let and directly change the value of the state without using the setState function and if you do you need to pre increment not post increment if you post increment it s not going to work properly "`}</p>
+      <button onClick={plusOne}>CLICK ME</button>
+
+      <p>{`"A slight note tho, you should probably not declare useState with let and directly change the value of the state without using the setState function and if you do you need to pre increment not post increment if you post increment it s not going to work properly "`}</p>
+      <br />
+      <h2>Initial Render and Re-Renders</h2>
+
+      
+      <p>Initial render is the moment the application loads for the first time, or the component tree loads for the first time in the DOM. This is also called mounting components</p>
+
+      <p>
+        Re-rendering triggers when:
+        <br /><br />
+        Re-renders are triggered when components state is updated or props change.
+        When we change state or props react will re-render to reflect these changes
+        <br /><br />
+        Re-render is also triggered when a parent element re-renders, even if state is not updated or props changed. Here I suppose if a parent element re-render then all elements wil re-render, it just makes sense.
+      </p>
     </section>
   )
 }
