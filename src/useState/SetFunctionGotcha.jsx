64db445d74a37153f4832f80e8ab7047aef71452
Gabriel <murariugabi23@gmail.com>
2023-06-07 08:08:54 +0300

    learned about updating state immediately and synchronously

--- src/useState/SetFunctionGotcha.jsx
+++ src/useState/SetFunctionGotcha.jsx
@@ -3,9 +3,29 @@ import { useState } from "react"
 const SetFunctionGotcha = () => {
   const [count, setCount] = useState(0)
 
+  const normalUpdate = () => {
+    setTimeout(() => {
+      setCount(count + 1)
+
+    },2000)
+    
+  }
+
   const add = () => {
-    setCount(count + 1)
-    console.log(count)
+    setCount((currentState => {
+      const newState = currentState + 1
+      console.log(count)
+      return newState
+    } ))
+  }
+
+  const timeOutEx = () => {
+    setTimeout(() => {
+      console.log("clicked the button")
+      setCount((currentState) => {
+        return currentState + 1
+      })
+    }, 3000)
   }
   console.log(count)
 
@@ -20,6 +40,20 @@ const SetFunctionGotcha = () => {
         the state and tells React that it needs to re-render component. The
         actual state update will be performed as part of the next rendering.
       </p>
+
+      <p>
+        <strong>On another note</strong> here we have another example but here
+        we are updating the state immediately and synchronously by updating the
+        previous state
+      </p>
+      <p>Count: {count}</p>
+      <button onClick={timeOutEx}>timed out add</button>
+
+      <p>
+        <strong>Here</strong> is the example in which we have a timeout but we just update state normally
+      </p>
+      <p>Count: {count}</p>
+      <button onClick={normalUpdate}>timed out add</button>
     </section>
   )
 }
