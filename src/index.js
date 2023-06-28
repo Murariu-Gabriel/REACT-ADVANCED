b8d678411e28ac31a281159e5d0b4ed5cb2fc04c
Gabriel <murariugabi23@gmail.com>
2023-06-28 08:05:17 +0300

    just added a little utility component

--- src/index.js
+++ src/index.js
@@ -1,23 +1,3 @@
-// import MultipleReturnFetchData from "./components/conditionalRendering/MultipleReturnFetchData"
-// import MultipleReturns from "./components/conditionalRendering/MultipleReturns"
-// import ProjectStructure from "./components/conditionalRendering/ProjectStructure"
-// import ShortCircuit from "./components/conditionalRendering/ShortCircuit"
-// import ShortCircuitUsualUseCase from "./components/conditionalRendering/ShortCircuitUsualUseCase"
-// import ToggleChallenge from "./components/conditionalRendering/ToggleChallenge"
-// import UserChallenge from "./components/conditionalRendering/UserChallenge"
-// import FetchData from "./components/useEffect/fetching-data/FetchData"
-// import MultipleEffects from "./components/useEffect/MultipleEffects"
-// import UseEffectBasics from "./components/useEffect/UseEffectBasics"
-// import ErrorExample from "./components/useState/ErrorExample"
-// import InfiniteStateLoop from "./components/useState/InfiniteStateLoop"
-// import SetFunctionGotcha from "./components/useState/SetFunctionGotcha"
-// import UseStateBasics from "./components/useState/UseStateBasics"
-// import UseStateWithArray from "./components/useState/UseStateWithArray"
-// import UseStateWithObject from "./components/useState/UseStateWithObject"
-// import ControlledInputs from "./components/forms/ControlledInputs"
-// import FormUserChallenge from "./components/forms/FormUserChallenge"
-// import MultipleInputs from "./components/forms/MultipleInputs"
-
 import UseStateMainComponent from "./components/useState/UseStateMainComponent";
 import UseEffectMainComponent from "./components/useEffect/UseEffectMainComponent";
 import ConditionalRenderingMainComponent from "./components/conditionalRendering/ShortCircuitMainComponent";
@@ -32,25 +12,3 @@ export {
   FormsMainComponent,
 }
 
-// export {
-//   MultipleReturnFetchData,
-//   MultipleReturns,
-//   ProjectStructure,
-//   ShortCircuit,
-//   ShortCircuitUsualUseCase,
-//   ToggleChallenge,
-//   UserChallenge,
-//   FetchData,
-//   MultipleEffects,
-//   UseEffectBasics,
-//   ErrorExample,
-//   InfiniteStateLoop,
-//   SetFunctionGotcha,
-//   UseStateBasics,
-//   UseStateWithArray,
-//   UseStateWithObject,
-//   List,
-//   ControlledInputs,
-//   FormUserChallenge,
-//   MultipleInputs,
-// }
