4f7621034db8af4740287af0bf6642cf78f5d797
Gabriel <murariugabi23@gmail.com>
2023-06-30 08:10:40 +0300

    played some more with useRef

--- src/components/useRef/UseRefBasics.jsx
+++ src/components/useRef/UseRefBasics.jsx
@@ -5,14 +5,15 @@ import { useState } from "react"
 const UseRefBasics = () => {
   const [value, setValue] = useState(0)
   const refContainer = useRef(null)
+  const isMounted = useRef(false)
 
-  console.log(refContainer)
-
+  
   useEffect(() => {
     refContainer.current.focus()
+    console.log(refContainer, "after initial render")
   })
-
-  const isMounted = useRef(false)
+  
+  console.log(refContainer, "during initial render")
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -53,6 +54,13 @@ const UseRefBasics = () => {
       <p>
         An here we have Use Ref, apparently used for preserving values between
         renders. Use Ref does not trigger re-renders and it targets dom elements.
+        <br /> <br />
+
+        In the last useEffect we have the case where we don t want to run some functionality on initial render and only on re-render. So we set up a useRef with the value false and check for falsy. It enters our if sets ref to true and exits without executing the rest of the code. And after that because the ref is set to true it will display content of useEffect on re-render
+
+        <br /> <br />
+
+        Important note for useEffect: UseEffect executes after initial render meaning no matter it s position inside the component it will execute last this also applies to any render/re-render use effects will execute last and depending on their order.
       </p>
     </section>
   )
