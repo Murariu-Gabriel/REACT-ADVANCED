0fbeac2d3db8ceb32088e764c99c9f2d73575267
Gabriel <murariugabi23@gmail.com>
2023-07-12 21:20:36 +0300

    Learned about useCallBack

--- src/components/performance/02-lower-state-challenge/index.jsx
+++ src/components/performance/02-lower-state-challenge/index.jsx
@@ -1,10 +1,15 @@
 import { data } from "../../../data"
-import { useState } from "react"
+import { useState, useCallback } from "react"
 import List from "./List"
 import Form from "./Form"
 
 const LowerStateChallenge = () => {
   const [people, setPeople] = useState(data)
+  const [data2, setData2] = useState([])
+
+  const handleData = useCallback(() => {
+    console.log(people)
+  }, [people])
 
   const addPerson = (name) => {
     const fakeId = Date.now()
@@ -34,6 +39,27 @@ const LowerStateChallenge = () => {
       <h2>React memo</h2>
 
       <p>React memo helps you memoize a component and it means that if the component didn t change it will not re-render, it will just returned the same component from the last render. It checks if the props have changed if not it will returned a cached result from last render</p>
+    
+      <h2>UseCallBack</h2>
+
+      <div>
+        <button onClick={handleData}>Click me</button>
+      </div>
+    
+
+    <p>Here we have a simple case of useCallBack which takes a function and a dependency array as parameters. UseCallBack memoizes a function and depending on the dependency array it will render or not the component when the value inside the dependency array changes. 
+
+      <br /> <br />
+      Bonus: a difference between useEffect and useCallBack is that they are used for different things and behave different. 
+      <br />
+      UseCallBack memoizes functions and returns a memoized version of the function that changes of the dependency for optimizing performance. 
+      <br />
+      UseEffect is used to perform side effects in components and it si usually used in fetching data or DOM manipulation. If the value inside the dependency changes then the logic inside useEffect will re-execute 
+      
+      
+      </p>
+
+
     </section>
   )
 }
